2e06a9d5a2e3b20b04b1f57855f75b2153ba3fd5
Evan Dwight <effdem3@gmail.com>
2021-09-26 11:20:26 -0600

    Show sortheader when there are no results

--- src/views/posts.tsx
+++ src/views/posts.tsx
@@ -3,36 +3,40 @@ import React from "react";
 import { DownVoteButton, Fields, HackerCommentsLink, UpVoteButton } from "./viewPost";
 import { QPost, User } from "../db/types";
 
-
-export const Posts = ({ posts, user, showCensored, moreLink, offset, sortType } : {posts: QPost[], user: User, showCensored: boolean, moreLink: string, offset: number, sortType: string}) => {
+const SortHeader = ({sortType}) =>
+    <div>
+        <b>{sortType}</b> | <a href={C.URLS.SORT}>change sort</a>
+    </div>;
+export const Posts = ({ posts, user, showCensored, moreLink, offset, sortType }: { posts: QPost[], user: User, showCensored: boolean, moreLink: string, offset: number, sortType: string }) => {
     if (posts.length === 0) {
-        return <div>No results found</div>;
+        return <div>
+            <SortHeader sortType={sortType}/>
+            <div>No results found</div>
+        </div>;
     }
     return <div>
-        <div>
-            <b>{sortType}</b> | <a href={C.URLS.SORT}>change sort</a>
-        </div>
+        <SortHeader sortType={sortType}/>
         <table id="posts">
-        {posts.filter(post => showCensored || !post.censor).map((post, i) => <Post key={i} i={i+offset} {... {post, user}}/>)}
+            {posts.filter(post => showCensored || !post.censor).map((post, i) => <Post key={i} i={i + offset} {... { post, user }} />)}
         </table>
-        {!!moreLink && 
+        {!!moreLink &&
             <div>
                 <a href={moreLink}>More</a>
             </div>}
     </div>
 }
 
-export const Post = ({post, i, user}: {post: QPost, i: number, user:User}) => {
+export const Post = ({ post, i, user }: { post: QPost, i: number, user: User }) => {
     return <tr className={i % 2 === 1 ? "bg-gray-200" : ""}>
         <td className="rank-col">
-                {i + 1}
+            {i + 1}
         </td>
         <td className="vote-col">
             <div>
-                <UpVoteButton {... {post, i}}/>
+                <UpVoteButton {... { post, i }} />
             </div>
             <div>
-                <DownVoteButton {... {post, i}}/>
+                <DownVoteButton {... { post, i }} />
             </div>
         </td>
         <td className="title-col">
@@ -42,13 +46,13 @@ export const Post = ({post, i, user}: {post: QPost, i: number, user:User}) => {
             <div className="gray-500">
                 by {post.user_name}
                 {!!post.url && <span> | <a href={post.url}>link</a></span>}
-                {!!post.hackernews_id && <span> | <HackerCommentsLink id={post.hackernews_id}/></span>}
+                {!!post.hackernews_id && <span> | <HackerCommentsLink id={post.hackernews_id} /></span>}
                 <span> | <a href={C.URLS.QPOSTS_VIEW + post.id}>more info</a></span>
                 {!!user && user.is_mod && <span> | <a href={C.URLS.QPOSTS_MOD_ACTIONS + post.id}>mod actions</a></span>}
             </div>
-            {hasInterestingFields(post) && 
+            {hasInterestingFields(post) &&
                 <div className="gray-500">
-                    <Fields post={post} showAllFields={false} showDisputes={false}/>
+                    <Fields post={post} showAllFields={false} showDisputes={false} />
                 </div>}
         </td>
     </tr>
