fda3b12f347bfecbeb306d88a5dddbf0d145a2f6
Evan Dwight <effdem3@gmail.com>
2021-11-04 13:03:04 -0600

    Update elected moderator description

--- src/views/about/elected-moderator.tsx
+++ src/views/about/elected-moderator.tsx
@@ -7,10 +7,13 @@ const element = () =>
     <div>
         <h2>{title}</h2>
         <p>
-            Sampling is still too expensive, so we use elected moderators as a first line of defense. This allows for timely action and prevents a flood of requests on everyone's time.
+            Our goal in moderation is to estimate a referendum. Moderators complement referendums because unlike referendums, moderators are timely and take only a single person's time.
         </p>
         <p>
-            Like sampling, moderators aren't perfect. Two mechanisms address this: first, users can change their vote at any time to kick the current moderator out of office, and second users can dispute individual moderator decisions in order to trigger a sample.
+            To align moderator decisions with the communitiy, moderatores are elected by the community. If the community doesn't like how the moderator acts, they can replace them. Further, if the community only disagrees with a single moderator descision they don't need to replace them. They can dispute that decision adn trigger a sample or referendum.
+        </p>
+        <p>
+            A single moderator gives accountability and decidability, but lacks the ability to judge millions of posts and has indiidual bias. To overcome these flaws we let the moderator designate as many mini moderators as needed. The mini mods vote how to moderate a post, and whatever the majority decides determines how the post is moderated. By designating a fraction of the community as mini mods, large volumes of posts can be moderated with care while also using the wisdom of the crowd to remove individual bias.
         </p>
     </div>
 
