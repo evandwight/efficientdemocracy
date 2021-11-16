a9a9687f049d92d31a839af0a383b09949253272
Evan Dwight <effdem3@gmail.com>
2021-11-16 11:27:34 -0600

    Add rss feed

---
+++ src/views/blog/rss.ts
@@ -0,0 +1,35 @@
+import { BlogEntry } from "./blogEntry"
+import * as C from '../../constant';
+
+export function rssXmlBoilerPlate(entries: BlogEntry[]) {
+    const lastModified = entries[0].created.toUTCString();
+    return `
+<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<channel>
+    <title>Efficient democracy blog</title>
+    <description>Development updates</description>
+    <link>https://efficientdemocracy.com/blog</link>
+    <copyright>Que?</copyright>
+    <lastBuildDate>${lastModified}</lastBuildDate>
+    <pubDate>${new Date(2021, 10, 16).toUTCString()}</pubDate>
+    <ttl>1440</ttl>
+    <atom:link href="https://efficientdemocracy.com/feed" rel="self" type="application/rss+xml" />
+
+${entries.map(rssItem).join("\n")}
+
+</channel>
+</rss>`
+}
+
+export function rssItem(entry: BlogEntry) {
+    const url = C.URLS.BASE_URL + entry.url;
+    return `
+<item>
+<title>${entry.title}</title>
+<link>${url}</link>
+<pubDate>${entry.created.toUTCString()}</pubDate>
+<guid>${url}</guid>
+</item>
+`;
+}
