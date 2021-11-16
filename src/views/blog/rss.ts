392ca7b7f2d943c2489a06e58359206ad056270e
Evan Dwight <effdem3@gmail.com>
2021-11-16 11:33:02 -0600

    Remove xml from rss

--- src/views/blog/rss.ts
+++ src/views/blog/rss.ts
@@ -4,7 +4,6 @@ import * as C from '../../constant';
 export function rssXmlBoilerPlate(entries: BlogEntry[]) {
     const lastModified = entries[0].created.toUTCString();
     return `
-<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
     <title>Efficient democracy blog</title>
