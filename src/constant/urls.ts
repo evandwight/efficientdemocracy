28ad8a7dd9ac5f191107a8cb12bc59fc18e2525a
Evan Dwight <effdem3@gmail.com>
2021-12-06 11:05:12 -0600

    Update url format

--- src/constant/urls.ts
+++ src/constant/urls.ts
@@ -1,62 +1,57 @@
 
 // QPosts
-export const QPOSTS_VIEW = "/qposts/view/";
-export const QPOSTS_VIEW_MOD_ACTION = "/qposts/view/mod-action/";
-export const SUBMIT_QPOST_VOTE = "/submit/qpost-vote/";
-export const SUBMIT_QPOST = "/submit/qpost";
-export const SUBMIT_QPOST_DISPUTE = "/submit/qpost-dispute/";
+export const QPOSTS_VIEW = "/post/";
+export const QPOSTS_VIEW_MOD_ACTION = "/mod-action/";
+export const SUBMIT_QPOST_VOTE = "/submit/vote/";
+export const SUBMIT_QPOST_DISPUTE = "/submit/dispute/";
 
-export const SORT = "/qposts/sort";
-export const PAST_NEWSLETTERS = "/qposts/past-newsletters";
+export const SORT = "/r/hackernews/sort";
+export const PAST_NEWSLETTERS = "/r/hackernews/past-newsletters";
 
-export const QPOSTS = "/hot-qposts";
-export const NEW_QPOSTS = "/new-qposts";
-export const DEEPLY_IMPORTANT_QPOSTS = "/deeply-important-qposts";
-export const TECHNICAL_QPOSTS = "/technical-and-hot";
-export const FROZEN_QPOSTS = "/frozen-qposts/";
-export const DISPUTED_QPOSTS = "/highly-disputed-qposts";
-export const MINI_MOD_QPOSTS = "/mini-mod-qposts";
+export const QPOSTS = "/r/hackernews/hot";
+export const NEW_QPOSTS = "/r/hackernews/new";
+export const DEEPLY_IMPORTANT_QPOSTS = "/r/hackernews/deeply-important";
+export const TECHNICAL_QPOSTS = "/r/hackernews/technical-and-hot";
+export const FROZEN_QPOSTS = "/r/hackernews/frozen/";
+export const DISPUTED_QPOSTS = "/r/hackernews/highly-disputed";
+export const MINI_MOD_QPOSTS = "/r/hackernews/mini-mod";
 
 // Samples
-export const VIEW_SAMPLE_REQUESTS = "/view/sample-requests";
-export const VIEW_SAMPLE_REQUEST = "/view/sample-request/";
+export const VIEW_SAMPLE_REQUESTS = "/sample-requests";
+export const VIEW_SAMPLE_REQUEST = "/sample-request/";
 export const SUBMIT_SAMPLE_VOTE = "/submit/sample-vote/";
 
 // Mod Vote
-export const VIEW_MOD_VOTE = "/view-mod-vote";
+export const VIEW_MOD_VOTE = "/change-mod-vote";
 export const SUBMIT_MOD_VOTE = "/submit/mod-vote/";
 
 // Proxy setting
-export const VIEW_PROXY = "/view-proxy";
+export const VIEW_PROXY = "/change-proxy";
 export const SUBMIT_PROXY = "/submit/proxy/";
 
-// Mods
-export const MODS_VIEW = "/mods";
-
 // QPosts Mods
-export const QPOSTS_MOD_ACTIONS = "/qposts/mod-actions/";
-export const SUBMIT_QPOST_MOD_ACTION = "/submit/post_mod_action/set/";
-export const MOD_VIEW_MINI_MODS = "/mod/view/mini-mods";
+export const QPOSTS_MOD_ACTIONS = "/mod/actions/";
+export const SUBMIT_QPOST_MOD_ACTION = "/mod/submit/action/set/";
+export const MOD_VIEW_MINI_MODS = "/mod/mini-mods";
 export const MOD_SUBMIT_SET_MINI_MOD = "/mod/submit/set-mini-mods/";
 
 // Mini Mods
-export const QPOSTS_MINI_MOD_ACTIONS = "/qposts/mini-mod-actions/";
-export const SUBMIT_QPOST_MINI_MOD_ACTION = "/submit/post-mini-mod-action/set/";
-
-// QComments
-// export const QCOMMENTS_VIEW = "/qcomments/view/";
-export const QCOMMENT_VIEW = "/qcomment/view/";
-export const SUBMIT_QCOMMENT = "/submit/qcomment";
+export const QPOSTS_MINI_MOD_ACTIONS = "/minimod/actions/";
+export const SUBMIT_QPOST_MINI_MOD_ACTION = "/minimod/submit/action/set/";
 
 // Accounts
 export const USER_LOGIN = "/user/login";
 export const USER_LOGOUT = "/user/logout";
 export const USER_STRIKES = "/user/strikes";
 export const USER_SETTINGS = "/user/settings";
-export const SUBMIT_USER_SETTINGS = "/submit/user/settings";
-export const SUBMIT_USER_FIRST_RUN = "/submit/user/first-run";
+export const SUBMIT_USER_SETTINGS = "/user/submit/settings";
+export const SUBMIT_USER_FIRST_RUN = "/user/submit/first-run";
 export const EMAIL_UNSUBSCRIBE = "/email/unsubscribe/";
 export const CHANGE_EMAIL = "/user/change-email"
+
+// Auth
+export const AUTH_COGNITO_CALLBACK = "/auth/cognito/callback";
+export const AUTH_COGNITO = "/auth/cognito";
 
 // Misc
 export const ABOUT = "/about/";
@@ -90,7 +85,3 @@ export const DISCORD = "https://discord.gg/7dSc2AA3C4";
 
 export const HN_COMMENT = "https://news.ycombinator.com/item?id=";
 export const BASE_URL = "https://efficientdemocracy.com";
-
-// Auth
-export const AUTH_COGNITO_CALLBACK = "/auth/cognito/callback";
-export const AUTH_COGNITO = "/auth/cognito";
