0bea0939f901987d004285acf66f39a06cb99672
Evan Dwight <effdem3@gmail.com>
2021-12-16 13:06:50 -0600

    Rewrite

--- src/views/about/democratic-moderation.tsx
+++ src/views/about/democratic-moderation.tsx
@@ -7,22 +7,25 @@ const element = () =>
     <div>
         <h2>{title}</h2>
         <p>
-            We want to make decisions by estimating a referendum. The following are tools to estimate a referendum described with how accurate, timely and cost in terms of uninterested user time:
+            We want to make decisions by estimating a referendum. We have the following tools:
         </p>
         <ul>
-            <li>Unsolicited votes - 0 cost, timely but inaccurate due to selection bias</li>
-            <li><a href={C.URLS.ABOUT_ELECTED_MODERATORS}>Elected representatives/moderator</a> - 0 cost and timely but inaccurate due to decisions only being influenced by incentives</li>
-            <li>Statistical sample - mostly accurate - sampling error - but constant cost per decision and not timely</li>
-            <li>Referendum - accurate but N cost per decision and not timely</li>
+            <li>Unsolicited votes - 0 cost* and timely but inaccurate due to selection bias</li>
+            <li><a href={C.URLS.ABOUT_ELECTED_MODERATORS}>Elected representatives</a> - 0 cost and timely but inaccurate due to only indirect influence over the decisions</li>
+            <li>Statistical sample - mostly accurate but constant cost and not timely</li>
+            <li>Referendum - accurate but N cost and not timely</li>
         </ul>
         <p>
-            Though each tool is fatally flawed, combining them allows us to overcome that. The efficiency of elected moderators lets them act as a first line of defense. They make the majority of the decisions and prevent a flood of requests on the community's time. We detect bad decisions with unsolicited votes - disputes - and then correct the decision by triggering a statistical sample. Similarly, statistical samples can be corrected by a referendum. The efficient corrected by the accurate.
+            * cost in terms of uninterested user time per decision
         </p>
         <p>
-            Disputes have a selection bias so they cannot be trusted to represent the will of the people. As such, we disincentivize abuse by empowering the community to give strikes to disputers when they feel the issue should not have been disputed.
+            Though each tool is fatally flawed, combining them allows us to overcome that. The efficiency of elected moderators lets them act as a first line of defense. They make the majority of decisions and prevent a flood of requests on the community's time. We detect bad decisions with unsolicited votes - disputes - and then correct the decision by triggering a statistical sample. Similarly, statistical samples can be corrected by a referendum. The efficient corrected by the accurate.
         </p>
         <p>
-            This system hybridizes a representative democracy with a direct democracy to share benefits and eliminate flaws.
+            Disputes have a selection bias so they can't be trusted to represent the will of the people. As such, we disincentivize abuse by empowering the community to give strikes to disputers when they feel the issue should not have been disputed.
+        </p>
+        <p>
+            This system hybridizes a representative democracy with a direct democracy to share benefits and minimize the flaws.
         </p>
         <h4>Let's work through an example:</h4>
         <ul>
--- src/views/about/what-is-this.tsx
+++ src/views/about/what-is-this.tsx
@@ -11,10 +11,10 @@ const Element = () =>
             Community curated news: keep up to date with hacker news without going down the rabbit hole.
         </p>
         <p>
-            Hacker news brings you the most engaging stories on the internet, but it doesn't know when to stop. You keep reading hoping the next comment will answer all your questions. Though usually it just makes you angry.
+            Hacker news doesn't know when to stop. You read the next comment hoping it will answer all your questions, but usually it just makes you angry.
         </p>
         <p>
-            Efficient Democracy converts endless scrolling to a weekly newsletter of just important stories. No more addiction. Only the news you need.
+            We convert endless scrolling to a weekly newsletter of just important stories. No more addiction. Only the news you need.
         </p>
         <p>
             They use your similarity to others to find what engages you. We use it to curate your news. If your peers marks it as important, so do we. We crowdsource curation, so you do not have to do it alone.
@@ -27,11 +27,11 @@ const Element = () =>
             To decide the importance of a post, we estimate the result of a referendum on “is this post important?”
         </p>
         <p>
-            Using consensus to curate. It's that simple!
+            Using consensus to curate.
         </p>
         <h3>But referendums can't handle the tsunami of posts!</h3>
         <p>
-            You are right. Referendums themselves take too much effort. We make <b>estimating</b> a referendum efficient. First, we use elected moderators to quickly categorize posts. Then we correct mistakes by using a statistical sample or a referendum on highly disputed categorizations. The efficient corrected by the accurate. For more details read about <a href={C.URLS.ABOUT_DEMOCRATIC_MODERATION}>how it works</a>.
+            You are right, referendums themselves take too much effort. We make <b>estimating</b> a referendum efficient. First, we use elected moderators to quickly categorize posts. Then for highly disputed categorizations, we correct mistakes with a statistical sample or a referendum. The efficient corrected by the accurate. For more details read about <a href={C.URLS.ABOUT_DEMOCRATIC_MODERATION}>how it works</a>.
         </p>
         <p>
             Want a community run by the community? <a href={SIGNUP}>Sign up</a>!
@@ -46,13 +46,13 @@ const Element = () =>
         <h3>We can estimate referendums on anything!</h3>
         <ul>
             <li>Should this post be quarantined?</li>
-            <li>Retrospectively, was this a good decision?</li>
             <li>Should this community be quarantined?</li>
+            <li>Retrospectively, was this a good decision?</li>
             <li>Is this person an expert in physics?</li>
-            <li>As an expert in physics, do you think this study is good?</li>
+            <li>As experts in physics, do you think this study is good?</li>
         </ul>
         <p>
-            The answers won't always be right but the goal is to improve the signal to noise ratio. A consensus is a good signal. By making decisions democratically we put them on firm foundations - community buy-in.
+            The answers won't always be right but the goal is to improve the signal to noise ratio. Consensus of you peers is a good signal. By making decisions democratically we put them on firm foundations - community buy-in.
         </p>
         <p>
             We do not need big tech monopolies to control speech. We can build communities that moderate themselves [22]. To learn more, read about <a href={C.URLS.ABOUT_HOW_WE_ARE_DIFFERENT}>how we are different</a>!
